05e207833f41bb127263ed12c4cc34b9a12b8c66
Haroon <harun.hairun@gmail.com>
2022-05-20 17:22:58 +0600

    Initial Commit

---
+++ homework1lesson1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework1lesson1
+//
+//  Created by Харун Хихиза on 20/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
