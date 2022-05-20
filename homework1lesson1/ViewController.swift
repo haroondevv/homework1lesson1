98d812e412ef361cc35364a5b46e3b1efefd5dfd
Haroon <harun.hairun@gmail.com>
2022-05-20 18:18:08 +0600

    add проект

--- homework1lesson1/ViewController.swift
+++ homework1lesson1/ViewController.swift
@@ -1,19 +1,52 @@
-//
-//  ViewController.swift
-//  homework1lesson1
-//
-//  Created by Харун Хихиза on 20/5/22.
-//
+//Домашнее задание №1.
+//Добавить 1 label, 3 обычных view, 1 кнопку. Расположить элементы по горизонтали по центру экрана, по вертикали друг за другом с отступами. По нажатию кнопки один из view должен закрашиваться в один из 3 цветов: зеленый, красный, желтый. Label также должен менять свое значение в зависимости от цвета.
+
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var view1: UIView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var view2: UIView!
+    
+    @IBOutlet weak var view3: UIView!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    enum Views {
+        case view1
+        case view2
+        case view3
+        }
+        var counter = 0
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            label.text = ""
+            }
+        
+    @IBAction func buttton(_ sender: UIButton) {
+        
+        counter += 1
+        switch counter {
+        case 1:
+            view3.backgroundColor = .white
+            view2.backgroundColor = .red
+            label.text = "Red"
+        case 2:
+            view2.backgroundColor = .white
+            view1.backgroundColor = .yellow
+            label.text = "Yellow"
+        case 3:
+            view1.backgroundColor = .white
+            view3.backgroundColor = .green
+            label.text = "Green"
+            counter = 0
+        default:
+            label.text = "nil"
+        }
     }
-
-
+    
 }
+
 
